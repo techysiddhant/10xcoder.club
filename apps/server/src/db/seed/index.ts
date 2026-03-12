5f6f8e5f95ec15249c3d5a2aa7c3264211076d8f
Siddhant Jain <sidjain8000@gmail.com>
2026-03-13 01:23:26 +0530

    feat(seed): add initial resource types and seeding functionality

---
+++ apps/server/src/db/seed/index.ts
@@ -0,0 +1,15 @@
+import { db } from "..";
+import { resourceType } from "@workspace/database";
+import { INITIAL_TYPES } from "./resourceType";
+let exitCode = 0;
+try {
+  for (const type of INITIAL_TYPES) {
+    await db.insert(resourceType).values(type).onConflictDoNothing();
+    console.log(`  ✓ ${type.name}`);
+  }
+} catch (error) {
+  console.error("🔴 Error seeding", error);
+  exitCode = 1;
+} finally {
+  process.exit(exitCode);
+}
