7ed6cdcf5b520dddbd235f4c132aeef487fcce61
Siddhant Jain <sidjain8000@gmail.com>
2026-03-26 14:56:32 +0530

    refactor(analytics): replace custom Google Analytics implementation with third-party component

--- apps/web/components/google-analytics.tsx
+++ apps/web/components/google-analytics.tsx
@@ -1,34 +1,12 @@
 "use client";
-
-import Script from "next/script";
+import { GoogleAnalytics as GoogleAnalyticsComponent } from "@next/third-parties/google";
 
 type GoogleAnalyticsProps = {
   gaId: string;
 };
 
 const GoogleAnalytics = ({ gaId }: GoogleAnalyticsProps) => {
-  return (
-    <>
-      <Script
-        id="gtag-init"
-        strategy="beforeInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            window.gtag = gtag;
-            gtag('js', new Date());
-            gtag('config', '${gaId}');
-          `,
-        }}
-      />
-      <Script
-        id="gtag-src"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-        strategy="afterInteractive"
-      />
-    </>
-  );
+  return <GoogleAnalyticsComponent gaId={gaId} />;
 };
 
 export default GoogleAnalytics;
